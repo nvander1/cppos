eaccf81e810885c89bc0727edb06607becf3ea53
Nikolas Vanderhoof <nikolasrvanderhoof@gmail.com>
2018-05-14 04:43:03 -0400

    Rework qemu to use kernel instead of iso

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
-. ./iso.sh
+#. ./iso.sh
+. ./build.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom cppos.iso
+# qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom cppos.iso
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel kernel/cppos.kernel
